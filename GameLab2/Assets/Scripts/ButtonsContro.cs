82af8aa46944bd085901565a9a36ebcea975ad93
ZeusAC <fabiociconi@yahoo.com.br>
2018-02-04 21:53:09 -0500

    df
    gff

---
+++ GameLab2/Assets/Scripts/ButtonsContro.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsContro : MonoBehaviour
+{
+
+
+    public GameObject fabioPerson;
+
+    public Sprite changeColor;
+
+    private SpriteRenderer sr;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        sr = fabioPerson.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+
+        sr.sprite = changeColor;
+
+    }
+    private void Update()
+    {
+
+    }
+}
